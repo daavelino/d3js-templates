7ab7ce681bd332f439df306fc16eecf684c3d263
Daniel Avelino <daavelino@gmail.com>
2021-08-18 19:34:05 +0200

    Fixing uid generation.

--- examples/treemap/modules/treemap.mjs
+++ examples/treemap/modules/treemap.mjs
@@ -20,7 +20,22 @@ function treemap(settings) {
     .attr("preserveAspectRatio", "xMinYMin meet")
     .attr("viewBox",[0, 0, svg_width, svg_height])
     .style("font", svg_font);
-  
+ 
+
+  var count = 0;
+  const uid = function(name) {
+    return new Id("O-" + (name == null ? "" : name + "-") + ++count);
+    }
+
+    function Id(id) {
+      this.id = id;
+      this.href = new URL(`#${id}`, location) + "";
+    }
+
+    Id.prototype.toString = function() {
+      return "url(" + this.href + ")";
+  };
+
   const svg = d3.select("svg");
 
   // Chart construction:
@@ -45,23 +60,26 @@ function treemap(settings) {
 	.attr("transform", d => `translate(${d.x0},${d.y0})`);
 
     leaf.append("rect")
-      .attr("fill", d => { while (d.depth > 1) d = d.parent; return color(d.data.name); })
-      .attr("fill-opacity", rect_fill_opacity)
-      .attr("width", d => d.x1 - d.x0)
-      .attr("height", d => d.y1 - d.y0);
-
-
-    leaf.append("text")
-      .selectAll("tspan")
-      .data(d => d.data.name.split(/(?=[A-Z][a-z])|\s+/g).concat(format(d.value)))
-      .join("tspan")
-        .attr("x", 3)
-        .attr("y", (d, i, nodes) => `${(i === nodes.length - 1) * 0.3 + 1.1 + i * 0.9}em`)
-        .attr("fill-opacity", (d, i, nodes) => i === nodes.length - 1 ? 0.6 : null)
-        .text(d => d);
-
-    leaf.append("title")
-      .text(d => `${d.ancestors().reverse().map(d => d.data.name).join("/")}\n${format(d.value)}`);
+          .attr("id", d => (d.leafUid = uid("leaf")).id)
+          .attr("fill", d => { while (d.depth > 1) d = d.parent; return color(d.data.name); })
+          .attr("fill-opacity", 0.6)
+          .attr("width", d => d.x1 - d.x0)
+          .attr("height", d => d.y1 - d.y0);
+    
+      leaf.append("clipPath")
+          .attr("id", d => (d.clipUid = uid("clip")).id)
+        .append("use")
+          .attr("xlink:href", d => d.leafUid.href);
+    
+      leaf.append("text")
+          .attr("clip-path", d => d.clipUid)
+        .selectAll("tspan")
+        .data(d => d.data.name.split(/(?=[A-Z][a-z])|\s+/g).concat(format(d.value)))
+        .join("tspan")
+          .attr("x", 3)
+          .attr("y", (d, i, nodes) => `${(i === nodes.length - 1) * 0.3 + 1.1 + i * 0.9}em`)
+          .attr("fill-opacity", (d, i, nodes) => i === nodes.length - 1 ? 0.7 : null)
+          .text(d => d);
 
 
     return svg.node();
