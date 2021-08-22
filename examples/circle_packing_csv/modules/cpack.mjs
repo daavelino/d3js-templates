20eea921b80ae88f5514d2ce34aaa5e3601a5348
Daniel Avelino <daavelino@gmail.com>
2021-08-22 23:25:18 +0200

    Premature Circle Packing reading from a csv file.

---
+++ examples/circle_packing_csv/modules/cpack.mjs
@@ -0,0 +1,119 @@
+import * as d3 from "https://cdn.skypack.dev/d3@7"
+
+
+function gen_cpack(settings) {
+
+  let svg_id = settings["html_layout"]["svg"]["id"];
+  let svg_width = settings["html_layout"]["svg"]["width"];
+  let svg_height = settings["html_layout"]["svg"]["height"];
+  let svg_font = settings["html_layout"]["svg"]["font"];
+  let html_title = settings["html_layout"]["title"]; 
+  let data_url = settings["data_url"];
+
+  d3.select("title").text(html_title);
+
+  d3.select("#chart")
+    .append("svg")
+    .attr("id",svg_id)
+    .attr("text-anchor", "middle")
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("viewBox",[0, 0, svg_width, svg_height])
+    .style("font", svg_font);
+  
+
+  var count = 0;
+  const uid = function(name) {
+    return new Id("O-" + (name == null ? "" : name + "-") + ++count);
+    }
+
+    function Id(id) {
+      this.id = id;
+      this.href = new URL(`#${id}`, location) + "";
+    }
+
+    Id.prototype.toString = function() {
+      return "url(" + this.href + ")";
+  };
+
+  const svg = d3.select(`#${svg_id}`);
+
+  // Chart construction:
+  d3.csv(data_url).then(function(data) {
+
+    const shadow = uid("shadow");
+  
+    const color = d3.scaleSequential([6, 0], d3.interpolateMagma);
+    const format = d3.format(",d");
+ 
+    const rollup = d3.rollup(
+      data,
+      v => d3.sum(v, d => d.counting),
+      d => d.root,
+      d => d.actor,
+      d => d.resource,
+      d => d.action,
+    );
+
+
+
+    const pack = data => d3.pack()
+      .size([svg_width - 2, svg_height - 2])
+      .padding(3)
+      .radius(d => d.value + 50)
+    (d3.hierarchy(rollup)
+      .sum(d => d.counting)
+      .sort((a, b) => b.value - a.value));
+  
+    const root = pack(rollup);
+  
+    svg.append("filter")
+        .attr("id", shadow.id)
+      .append("feDropShadow")
+        .attr("flood-opacity", 0.3)
+        .attr("dx", 0)
+        .attr("dy", 1);
+  
+    const node = svg.selectAll("g")
+      .data(d3.group(root.descendants(), d => d.height))
+      .join("g")
+        .attr("filter", shadow)
+      .selectAll("g")
+      .data(d => d[1])
+      .join("g")
+        .attr("transform", d => `translate(${d.x + 1},${d.y + 1})`);
+  
+    node.append("circle")
+        .attr("r", d => d.r)
+        .attr("fill", d => color(d.height));
+    node.append("title")
+        .text(d => `${d.data[0]}`);
+ 
+    const leaf = node.filter(d => !d.children);
+
+    leaf.select("circle")
+        .attr("id", d => (d.leafUid = uid("leaf")).id);
+  
+    leaf.append("clipPath")
+        .attr("id", d => (d.clipUid = uid("clip")).id)
+      .append("use")
+        .attr("xlink:href", d => d.leafUid.href);
+  
+    leaf.append("text")
+      .attr("clip-path", d => d.clipUid)
+     .selectAll("tspan")
+     .data(d => d.data)
+       .join("tspan")
+         .attr("x", 0)
+         .attr("y", (d, i, nodes) => `${i - nodes.length / 2 + 0.8}em`)
+        .text(d => d);
+
+    node.append("title")
+        .text(d => d);
+      
+    return svg.node();
+  
+  });
+
+}
+
+export { gen_cpack };
