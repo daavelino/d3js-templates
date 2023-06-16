e45d9ae6cc44fb540e93889f0a56635133e63f61
Daniel Avelino <daavelino@gmail.com>
2023-06-16 11:19:04 +0200

    Update flarify.py

--- python/flarify/flarify.py
+++ python/flarify/flarify.py
@@ -4,19 +4,36 @@ import sys
 def parse_input():
     result = {
         "input_file": None,
-        "output_file": None
+        "output_file": None,
+        "root_node": None
     }
+    usage_msg = f"Usage: python {sys.argv[0]} -i <input.json> -o [output.json] -r [root_node name]. Exiting."
 
     if len(sys.argv) < 2:
-        print(f"Usage: python {sys.argv[0]} <input.json> [output.json]. Exiting.")
+        print(usage_msg)
         sys.exit(1)
 
-    result["input_file"] = sys.argv[1]
+    try:
+        idx = sys.argv.index("-i")
+        if idx:
+            result["input_file"] = sys.argv[idx + 1]
+    except:
+        print(usage_msg)
+        sys.exit(1)
 
-    if len(sys.argv) == 3:
-        result["output_file"] = sys.argv[2]
-    else:
+    try:
+        idx = sys.argv.index("-o")
+        if idx:
+            result["output_file"] = sys.argv[idx + 1]
+    except:
         result["output_file"] = "ffd-" + result["input_file"]
+
+    try:
+        idx = sys.argv.index("-r")
+        if idx:
+            result["root_node"] = sys.argv[idx + 1]
+    except:
+        result["root_node"] = "ROOT_NODE"
 
     return result
 
@@ -42,11 +59,13 @@ def run():
 
     filename = tmp["input_file"]
     output = tmp["output_file"]
+    root_node = tmp["root_node"]
+
     with open(filename) as f:
         data = f.read()
         hierarchy = json.loads(data)
 
-    result = flarify(hierarchy, "ROOT NODE")
+    result = flarify(hierarchy, root_node)
 
     with open(output, "w", encoding="utf-8") as f:
         json.dump(result, f)#, ensure_ascii=False, indent=4)
@@ -56,3 +75,5 @@ def run():
 
 if __name__=="__main__":
     run()
+                                     
+
