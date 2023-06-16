7bd93b68058691f01b0b515734a6cf49bca5f51d
Daniel Avelino <daavelino@gmail.com>
2023-06-16 09:57:04 +0200

    Update flarify.py

--- python/flarify/flarify.py
+++ python/flarify/flarify.py
@@ -1,7 +1,24 @@
 import json
-from pprint import pprint
+import sys
 
+def parse_input():
+    result = {
+        "input_file": None,
+        "output_file": None
+    }
 
+    if len(sys.argv) < 2:
+        print(f"Usage: python {sys.argv[0]} <input.json> [output.json]. Exiting.")
+        sys.exit(1)
+
+    result["input_file"] = sys.argv[1]
+
+    if len(sys.argv) == 3:
+        result["output_file"] = sys.argv[2]
+    else:
+        result["output_file"] = "ffd-" + result["input_file"]
+
+    return result
 
 def flarify(hierarchy, root_name):
     """ Provided a JSON hierarchy, converts it to a flare-2 format."""
@@ -20,12 +37,22 @@ def flarify(hierarchy, root_name):
     return result
 
 
-if __name__=="__main__":
-    filename = "example.json"
+def run():
+    tmp = parse_input()
+
+    filename = tmp["input_file"]
+    output = tmp["output_file"]
     with open(filename) as f:
         data = f.read()
         hierarchy = json.loads(data)
 
     result = flarify(hierarchy, "ROOT NODE")
 
-    print(result)
+    with open(output, "w", encoding="utf-8") as f:
+        json.dump(data, f)#, ensure_ascii=False, indent=4)
+
+    print(f"Done. Please check the flarified file at {output}.")
+
+
+if __name__=="__main__":
+    run()
